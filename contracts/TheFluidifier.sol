cf4ccc1fd9df3ec41c1ad9a6176291c3d3b39ff0
grcasanova <giammarco@MacBook-Pro-di-Giammarco.local>
2021-05-02 12:56:58 +0200

    added metadata

--- contracts/TheFluidifier.sol
+++ contracts/TheFluidifier.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 pragma solidity >=0.7.0 <0.9.0;
 
 import { ILendingPool } from "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
--- src/pages/sell/sell.jsx
+++ src/pages/sell/sell.jsx
@@ -37,16 +37,16 @@ export default class Sell extends Component {
                         <form className="needs-validation">
                             <div className="row">
                                 <div className="col-md-6 mb-3">
-                                    <label for="firstName">First name</label>
-                                    <input autofocus type="text" className="form-control" id="firstName" placeholder="John" min="2" max="50" required />
+                                    <label htmlFor="first_name">First name</label>
+                                    <input autoFocus type="text" className="form-control" id="first_name" placeholder="John" min="2" max="50" required />
                                     <div className="invalid-feedback">
                                         A valid first name is required.
                                     </div>
                                 </div>
 
                                 <div className="col-md-6 mb-3">
-                                    <label for="lastName">Last name</label>
-                                    <input type="text" className="form-control" id="lastName" placeholder="Doe" min="2" max="50" required />
+                                    <label htmlFor="last_name">Last name</label>
+                                    <input type="text" className="form-control" id="last_name" placeholder="Doe" min="2" max="50" required />
                                     <div className="invalid-feedback">
                                         A valid last name is required.
                                     </div>
@@ -54,7 +54,7 @@ export default class Sell extends Component {
                             </div>
 
                             <div className="mb-3">
-                                <label for="address">Address</label>
+                                <label htmlFor="address">Address</label>
                                 <div className="input-group">
                                 <input type="text" className="form-control" id="address" placeholder="1234 Main St" min="3" max="100" required />
                                     <div className="invalid-feedback">
@@ -65,7 +65,7 @@ export default class Sell extends Component {
 
                             <div className="row">
                                 <div className="col-md-5 mb-3">
-                                    <label for="country">Country</label>
+                                    <label htmlFor="country">Country</label>
                                     <CountryDropdown value={country} onChange={(val) => this.selectCountry(val)} 
                                     className="custom-select d-block w-100" id="country" required />
                                     <div className="invalid-feedback">
@@ -73,15 +73,15 @@ export default class Sell extends Component {
                                     </div>
                                 </div>
                                 <div className="col-md-4 mb-3">
-                                    <label for="state">State</label>
+                                    <label htmlFor="region">Region</label>
                                     <RegionDropdown country={country} value={region} 
-                                    onChange={(val) => this.selectRegion(val)} className="custom-select d-block w-100" id="state" required />
+                                    onChange={(val) => this.selectRegion(val)} className="custom-select d-block w-100" id="region" required />
                                     <div className="invalid-feedback">
-                                        Please provide a valid state.
+                                        Please provide a valid region.
                                     </div>
                                 </div>
                                 <div className="col-md-3 mb-3">
-                                    <label for="zip">Zip</label>
+                                    <label htmlFor="zip">Zip</label>
                                     <input type="text" className="form-control" id="zip" placeholder="012..." required />
                                     <div className="invalid-feedback">
                                         Zip code required.
@@ -94,7 +94,7 @@ export default class Sell extends Component {
                             <h4 className="mb-3">Other Data</h4>
 
                             <div className="mb-3">
-                                <label for="title">Title</label>
+                                <label htmlFor="title">Title</label>
                                 <input type="text" className="form-control" id="title" placeholder="A catchy title" min="3" max="100" required />
                                 <div className="invalid-feedback">
                                     Title is required
@@ -102,8 +102,8 @@ export default class Sell extends Component {
                             </div>
 
                             <div className="mb-3">
-                                <label for="description">Description</label>
-                                <textarea className="form-control" id="description" placeholder="Tell us more about the property to let" min="10" max="500" spellcheck="true" required></textarea>
+                                <label htmlFor="description">Description</label>
+                                <textarea className="form-control" id="description" placeholder="Tell us more about the property to let" min="10" max="500" spellCheck="true" required></textarea>
                                 <div className="invalid-feedback">
                                 Description is required
                                 </div>
@@ -111,24 +111,39 @@ export default class Sell extends Component {
 
                             <div className="row">
                                 <div className="col-md-5 mb-3">
-                                    <label for="surface">Surface</label>
-                                    <input type="number" className="form-control" id="surface" placeholder="10 m2" required />
-                                    <div className="invalid-feedback">
-                                        Please select a valid number.
+                                    <label htmlFor="surface">Surface</label>
+                                    <div className="input-group mb-3">
+                                        <input type="number" className="form-control" id="surface" placeholder="10" required />
+                                        <div className="input-group-append">
+                                            <span className="input-group-text" id="basic-addon1">m2</span>
+                                        </div>
+                                        <div className="invalid-feedback">
+                                            Please select a valid number.
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="col-md-4 mb-3">
-                                    <label for="price">Price/month (USD)</label>
-                                    <input type="number" className="form-control" id="price" placeholder="$850" min="1" max="10000" required />
-                                    <div className="invalid-feedback">
-                                        Please provide a valid number.
+                                    <label htmlFor="price">Price/month (USD)</label>
+                                    <div className="input-group mb-3">
+                                        <div className="input-group-prepend">
+                                            <span className="input-group-text" id="basic-addon2">$</span>
+                                        </div>
+                                        <input type="number" className="form-control" id="price" placeholder="850" min="1" max="10000" required />
+                                        <div className="invalid-feedback">
+                                            Please provide a valid number.
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="col-md-3 mb-3">
-                                    <label for="deposit">Deposit</label>
-                                    <input type="number" className="form-control" id="deposit" placeholder="$200" min="0" max="10000" required />
-                                    <div className="invalid-feedback">
-                                        Please choose a valid option.
+                                    <label htmlFor="deposit">Deposit (USD)</label>
+                                    <div className="input-group mb-3">
+                                        <div className="input-group-prepend">
+                                            <span className="input-group-text" id="basic-addon3">$</span>
+                                        </div>
+                                        <input type="number" className="form-control" id="deposit" placeholder="200" min="0" max="10000" required />
+                                        <div className="invalid-feedback">
+                                            Please choose a valid option.
+                                        </div>
                                     </div>
                                 </div>
                             </div>
