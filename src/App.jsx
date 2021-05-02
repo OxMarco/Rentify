54e3681c46b76beb57a74df532fbf5b4b146873b
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2021-05-02 18:44:15 +0100

    Refactor web3 instanciation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Torus from "@toruslabs/torus-embed";
+import Web3 from "web3";
+
 import Home from './pages/home/home';
 import Gallery from './pages/gallery/gallery';
 import Info from './pages/info/info';
@@ -11,23 +14,43 @@ export default class App extends Component {
 
         this.state = {
             address: '',
+            web3: null,
         };
 
-        this.handleLogin = this.handleLogin.bind(this);
+        this.login = this.login.bind(this);
     }
 
-    handleLogin(walletAddress) {
-        this.setState({address: walletAddress});
+    async login() {
+        const torus = new Torus({});
+        await torus.init({
+            enableLogging: false,
+        });
+        await torus.setProvider({
+            host: 'goerli',
+        });
+
+        await torus.login();
+
+        const web3 = new Web3(torus.provider);
+        const address = (await web3.eth.getAccounts())[0];
+
+        const userInfo = await torus.getUserInfo();
+        console.log(userInfo);
+
+        this.setState({
+            address: address,
+            web3: web3,
+        });
     }
 
     render() {
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/sell" render={(_props) => <Sell address={this.state.address} onLogin={this.handleLogin} />} />
-                    <Route exact path="/gallery" render={(_props) => <Gallery address={this.state.address} onLogin={this.handleLogin} />} />
-                    <Route exact path="/info/:id" render={(_props) => <Info address={this.state.address} onLogin={this.handleLogin} />} />
-                    <Route exact path="/" render={(_props) => <Home address={this.state.address} onLogin={this.handleLogin} />} />
+                    <Route exact path="/sell" render={(_props) => <Sell address={this.state.address} login={this.login} />} />
+                    <Route exact path="/gallery" render={(_props) => <Gallery address={this.state.address} login={this.login} />} />
+                    <Route exact path="/info/:id" render={(_props) => <Info address={this.state.address} web3={this.state.web3} login={this.login} />} />
+                    <Route exact path="/" render={(_props) => <Home address={this.state.address} login={this.login} />} />
                 </Switch>
             </BrowserRouter>
         );
